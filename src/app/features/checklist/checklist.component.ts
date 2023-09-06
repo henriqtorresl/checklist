f140271d78cf3238be297e9dba0003bc1bddfcae
henriqtorresl <henriquetlandin@gmail.com>
2023-09-06 18:50:51 -0300

    lógica do paginator implementada

--- src/app/features/checklist/checklist.component.ts
+++ src/app/features/checklist/checklist.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Input, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { Tarefa } from 'src/app/models/tarefa.model';
 import { AdicionarTarefaDialogComponent } from '../dialogs/adicionar-tarefa-dialog/adicionar-tarefa-dialog.component';
@@ -6,15 +6,17 @@ import { TarefaService } from '../services/tarefa.service';
 import { take, tap } from 'rxjs';
 import { EditarTarefaDialogComponent } from '../dialogs/editar-tarefa-dialog/editar-tarefa-dialog.component';
 import { DeletarTarefaDialogComponent } from '../dialogs/deletar-tarefa-dialog/deletar-tarefa-dialog.component';
+import { PageEvent } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-checklist',
   templateUrl: './checklist.component.html',
   styleUrls: ['./checklist.component.scss']
 })
-export class ChecklistComponent implements OnInit {
+export class ChecklistComponent implements OnInit, AfterViewInit {
 
   @Input() tarefas: Tarefa[] = [];
+  tarefasFiltradas: Tarefa[] = [];
 
   constructor(
     private dialog: MatDialog,
@@ -22,9 +24,18 @@ export class ChecklistComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-
   }
   
+  ngAfterViewInit(): void {
+    this.tarefas.forEach((tarefa, index) => {
+      if (index < 4) {
+        this.tarefasFiltradas.push(tarefa);
+      }
+    })
+
+    console.log(this.tarefasFiltradas);
+  }
+
   abrirDialogAdicionar(): void {
     const dialogRef = this.dialog.open(AdicionarTarefaDialogComponent, {});
 
@@ -86,4 +97,41 @@ export class ChecklistComponent implements OnInit {
     ).subscribe();
   }
 
+  // o "$" antes do event, no html diz para a aplicação angular que o que estou recebendo nesse método é um evento 
+  mostrarTarefas(event: PageEvent) {    // função responsável pela lógica do paginator
+    console.log('evento emitido!');
+    const pageIndex = event.pageIndex;
+    
+    if (pageIndex == 0) {
+
+      this.tarefasFiltradas.splice(0, this.tarefasFiltradas.length);  // limpa a lista filtrada
+      this.tarefas.forEach((tarefa, index) => {
+        if (index < 4) {
+          this.tarefasFiltradas.push(tarefa);
+        }
+      });
+
+    } else {
+
+      let index = 1;
+      let qnt = 4;
+      let contador = 8;
+
+      while (qnt < this.tarefas.length) {
+        if (pageIndex == index) {
+          this.tarefasFiltradas.splice(0, this.tarefasFiltradas.length);
+          this.tarefas.forEach((tarefa, index) => {
+            if (index >= qnt && index < contador) {
+              this.tarefasFiltradas.push(tarefa);
+            }
+          });
+        }
+
+        index++; 
+        qnt += 4;
+        contador += 4;
+
+      }
+    }
+  }
 }
